80c56f1046c1ea89e2e555ba2a7a44155922ea49
mondos24 <edolnov@mail.ru>
2023-11-11 02:48:11 +0400

    Hotkey and some changes

--- js/background.js
+++ js/background.js
@@ -17,17 +17,22 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
 
 let filterEnabled = false;
 
+function toggleFilter() {
+    filterEnabled = !filterEnabled;
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        var message = (filterEnabled) ? {applyFilter: true} : {removeFilter: true};
+        chrome.tabs.sendMessage(tabs[0].id, message);
+    });
+}
+
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     if (request.toggleFilter) {
-        filterEnabled = !filterEnabled;
-        if (filterEnabled) {
-            chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-                chrome.tabs.sendMessage(tabs[0].id, {applyFilter: true});
-            });
-        } else {
-            chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-                chrome.tabs.sendMessage(tabs[0].id, {removeFilter: true});
-            });
-        }
+        toggleFilter();
+    }
+});
+
+chrome.commands.onCommand.addListener((command) => {
+    if (command === "switch_blur") {
+        toggleFilter();
     }
 });
--- js/popup.js
+++ js/popup.js
@@ -1,14 +1,8 @@
 document.getElementById('toggleButton').addEventListener('click', function() {  // Слушаем кнопку в popup html
+    
     chrome.runtime.sendMessage({toggleFilter: true});
-
     var button = this;
-    if (button.classList.contains("btn-outline-success")) {
-      button.classList.remove("btn-outline-success");
-      button.classList.add("btn-outline-danger");
-      button.textContent = "Off";
-    } else {
-      button.classList.remove("btn-outline-danger");
-      button.classList.add("btn-outline-success");
-      button.textContent = "On";
-    }
+    button.classList.toggle("btn-outline-success");
+    button.classList.toggle("btn-outline-danger");
+    button.textContent = button.classList.contains("btn-outline-success") ? "On" : "Off";
 });
